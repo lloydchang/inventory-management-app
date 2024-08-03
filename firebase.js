2ac5412f48aec6476fc42bd76dd1f94d6cc5ca1e
lloydchang <lloydchang@gmail.com>
2024-08-02 18:00:51 -0700

    feat(firebase.js): add actual Firebase configuration
    replacing the placeholder values
    with your actual Firebase configuration
    
    https://discord.com/channels/958775364962889859/1265793944361373773/1268313134188466256
    
    Bill — 07/31/2024 2:03 PM PDT
    
    Just wanted to address this since lots of people seems to have an issue with this.
    
    Firebase keys are NOT meant to be private: https://firebase.google.com/support/guides/security-checklist#:~:text=API%20keys%20for%20Firebase%20services,done%20using%20Firebase%20Security%20Rules.
    
    Ofc you can, but since it’s a client side key, doesn’t do much anyways.
    
    For future projects that have server sided secrets, how to hide those will be shown to you.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
- apiKey: "YOUR_API_KEY",
- authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
- projectId: "YOUR_PROJECT_ID",
- storageBucket: "YOUR_PROJECT_ID.appspot.com",
- messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
- appId: "YOUR_APP_ID"
+ apiKey: "AIzaSyDW9CZh1_DTJj3oJEnt7Hga8rP99CLP2Fo",
+ authDomain: "inventory-management-app-7b7fd.firebaseapp.com",
+ projectId: "inventory-management-app-7b7fd",
+ storageBucket: "inventory-management-app-7b7fd.appspot.com",
+ messagingSenderId: "492036951422",
+ appId: "1:492036951422:web:10c4af38992963fec9f72f",
  };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
